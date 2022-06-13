722cf06feabf718ccedfe24776732cd19fba384f
magicminglee <magicminglee@gmail.com>
2022-06-13 16:01:28 +0800

    bsoncxx&bson cmake module

---
+++ src/cmake_modules/FindBsoncxx.cmake
@@ -0,0 +1,25 @@
+find_path(BSONCXX_INCLUDE_DIRS
+          NAMES bsoncxx/json.hpp
+          DOC "bsoncxx include dir"
+          PATH_SUFFIXES mongocxx/include/bsoncxx/v_noabi)
+
+find_library(BSONCXX_LIBRARIES NAMES bsoncxx DOC "bsoncxx library" PATH_SUFFIXES mongocxx/lib)
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(Bsoncxx
+                                  DEFAULT_MSG
+                                  BSONCXX_INCLUDE_DIRS
+                                  BSONCXX_LIBRARIES)
+mark_as_advanced(BSONCXX_INCLUDE_DIRS BSONCXX_LIBRARIES)
+
+if(Bsoncxx_FOUND)
+  if (NOT TARGET Bsoncxx_lib)
+    add_library(Bsoncxx_lib INTERFACE IMPORTED)
+  endif()
+  set_target_properties(Bsoncxx_lib
+                        PROPERTIES INTERFACE_INCLUDE_DIRECTORIES
+                                   "${BSONCXX_INCLUDE_DIRS}"
+                                   INTERFACE_LINK_LIBRARIES
+                                   "${BSONCXX_LIBRARIES}")
+
+endif(Bsoncxx_FOUND)
--- src/cmake_modules/FindMongoc.cmake
+++ src/cmake_modules/FindMongoc.cmake
@@ -23,29 +23,3 @@ if(Mongoc_FOUND)
                                    "${MONGOC_LIBRARIES}")
 
 endif(Mongoc_FOUND)
-
-find_path(BSON_INCLUDE_DIRS
-          NAMES bson.h
-          DOC "bson include dir"
-          PATH_SUFFIXES mongoc/include/libbson-1.0)
-
-find_library(BSON_LIBRARIES NAMES bson-1.0 DOC "bson library" PATH_SUFFIXES mongoc/lib)
-
-include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(Bson
-                                  DEFAULT_MSG
-                                  BSON_INCLUDE_DIRS
-                                  BSON_LIBRARIES)
-mark_as_advanced(BSON_INCLUDE_DIRS BSON_LIBRARIES)
-
-if(Bson_FOUND)
-  if (NOT TARGET Bson_lib)
-    add_library(Bson_lib INTERFACE IMPORTED)
-  endif()
-  set_target_properties(Bson_lib
-                        PROPERTIES INTERFACE_INCLUDE_DIRECTORIES
-                                   "${BSON_INCLUDE_DIRS}"
-                                   INTERFACE_LINK_LIBRARIES
-                                   "${BSON_LIBRARIES}")
-
-endif(Bson_FOUND)
